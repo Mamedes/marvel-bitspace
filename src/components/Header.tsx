9e7571e65d869cd2217bfdd23114ce28f3be02e0
Mamedes <mamedeser@gmail.com>
2021-03-20 12:20:02 -0400

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+
+const Header = () => {
+    return (
+        <header className="marvelHeader">
+            Marvel Heroes
+        </header>
+    )
+}
+export default Header
