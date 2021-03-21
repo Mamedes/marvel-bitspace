c59766fb139361494369df1bd37b02b8c757974b
Mamedes <mamedeser@gmail.com>
2021-03-21 17:19:15 -0400

    Update add styled-components styles.css and styles.ts

---
+++ src/components/header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderTop = styled.div`
+  height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 24px;
+  background: #ffffff;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);
+`;
