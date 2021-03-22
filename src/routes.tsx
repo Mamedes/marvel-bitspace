8ebab392976a5424005bbf6cbbcf647c45e8a412
Mamedes <mamedeser@gmail.com>
2021-03-21 23:24:53 -0400

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route } from "react-router-dom";
+import { HeroDetail } from './pages/HeroDetail';
 import { HomePage } from './pages/HomePage'
 
 
@@ -7,6 +8,7 @@ function Routes(){
   return(
     <BrowserRouter>
      <Route exact path="/:page(\d+)?" component={HomePage} />
+     <Route path="/heroes/:id(\d+)" component={HeroDetail} />
 
 
     </BrowserRouter>
