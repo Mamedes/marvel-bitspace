6bc4f07c81e086e40a263be66aeebb3aee8f222c
Mamedes <mamedeser@gmail.com>
2021-03-21 23:03:28 -0400

    Update index.tsx and styles.ts

---
+++ src/components/Heroes/index.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as api from '../../api/api';
+import { useErrorBoundary } from '../../utils/useErrorBoundary';
+import { config } from '../../config';
+import {Container, CardArea, CardAreaColumn, HeroCard, HeroCardWrapper, HeroNome, HeroImage, CardFooter,CardButton}from './styles'
+
+function renderHeroes(characters: api.Characters) {
+  return (
+    <Container>  
+           <CardArea>
+            {characters.map(character => (
+           
+              <CardAreaColumn>
+              <HeroCard >
+              <HeroCardWrapper key={character.id}>
+                <HeroImage>
+                  <img
+                    src={`${character.thumbnail.path+"/portrait_fantastic"}.${character.thumbnail.extension}`}
+                    alt={character.name}
+                    className="heroImage"
+                    style={{
+                      flexShrink: 0,
+                      height: 'auto'
+                    }}
+                  />
+                </HeroImage>
+                <HeroNome>
+                  <h5>{character.name}</h5>
+                </HeroNome>
+                <CardFooter>
+                  <CardButton as={Link}  to={`/heroes/${character.id}`}>
+                    Mais detalhes
+                  {/* <Link to={`/heroes/${character.id}`}>
+                    <span>Mais detalhe</span></Link> */}
+                  </CardButton>
+                 
+                </CardFooter>
+              </HeroCardWrapper>
+              </HeroCard>
+              </CardAreaColumn>
+       
+            ))}
+          </CardArea>
+      </Container>
+   
+  );
+}
+
+interface Props {
+  page: number;
+}
+
+export function Heroes(props: Props) {
+  const errorBoundary = useErrorBoundary();
+
+  const [characters, setCharacters] = useState<api.Characters>();
+  const { page } = props;
+
+  useEffect(() => {
+    async function fetch(_page: number) {
+
+      setCharacters(undefined);
+      try {
+        const _characters = await api.fetchCharacters(_page * config.nbCharactersPerPage);
+        setCharacters(_characters);
+      } catch (e) {
+        errorBoundary(e);
+      }
+    }
+
+    fetch(page);
+  }, [page, errorBoundary]);
+
+  if (characters === undefined) {
+    return <p>Aguarde Carregando...</p>;
+  }
+  if (characters.length === 0) {
+    return <p>Não há resultados para ser exibidos :(</p>;
+  }
+  return renderHeroes(characters);
+}
---
+++ src/components/Heroes/styles.ts
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { shade } from 'polished'
+
+export const Container = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    padding: 20px;
+    margin-top: 40px;
+    width: 100%;
+    overflow: auto;
+    height: auto;
+    height: calc(100vh -150px);
+  }
+`;
+export const CardArea = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(254px,1fr));
+    grid-gap:30px
+  }
+`;
+export const CardAreaColumn = styled.div`
+    flex: 1 ;
+    border-radius: 3px;
+    padding:  20px;
+    align-items: center;
+    position: relative;
+  }
+`;  
+export const HeroCard = styled.div`
+display: center;
+align-items: center;
+justify-content: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    display: flex;
+    border-radius:  6px;
+    width: 100%;
+    flex-direction: column;
+    
+  }
+`;
+export const HeroCardWrapper = styled.section`
+    display: block;
+    align-items: center;
+    
+  }
+`;    
+export const HeroNome = styled.div`
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 16px;
+  }
+`;  
+export const HeroImage = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img{    
+    width: 168px;
+    height: 256px;
+    border-radius: 10px;
+    
+  }
+`;  
+
+export const CardFooter = styled.div`
+  display:flex;
+  height: 100%;
+  padding: .75rem 1.25rem;  
+    
+  }
+`; 
+
+export const CardButton = styled.button`
+  background: white;
+  color:#FFFFFF;
+  text-transform: uppercase;
+  font-weight: bold;  
+  text-decoration:none;
+  padding: 0.25em 1em; 
+  font-size: 16px;
+  background: #AF1F24 0% 0% no-repeat padding-box;
+  border-radius: 0px 3px 3px 0px;
+  text-align: center;
+  letter-spacing: 0px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${shade(0.2, '#AF1F24')};
+  }
+
+`; 
+
+
+  
